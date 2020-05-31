6c6ef111ea04b3ff1318be8e171f8fe59661b2b8
razikusman <razikusman@gmail.com>
2020-05-31 07:16:47 +0530

    student details add to user table php file added

--- elearn/elearn php/pview.php
+++ elearn/elearn php/pview.php
@@ -1,15 +1,34 @@
 <?php 
 	require 'connect.php';
+	$user = [];
 	$parent = [];
-	$sql = "SELECT * FROM `parent`";
-
-	if($result = mysqli_query($con,$sql))
+	$sql1 = "SELECT * FROM `user`";
+	if($result = mysqli_query($con,$sql1))
 	{
 		$cr = 0;
 		while($row = mysqli_fetch_assoc($result)){
-			$parent[$cr]['NIC'] = $row['NIC'];
-			$parent[$cr]['student'] = $row['sID'];
-			$parent[$cr]['Contact number'] = $row['pContact'];
+			$user[$cr]['NIC'] = $row['uID'];
+			//$user[$cr]['password'] = $row['uPassword'];
+			$user[$cr]['user'] = $row['uType'];
+			$cr++;
+		}
+		
+		echo json_encode($user);
+	}
+	else{
+		http_response_code(404);
+	}
+	
+	//////////////////
+	
+	$sql2 = "SELECT * FROM `parent`";
+	if($result = mysqli_query($con,$sql2))
+	{
+		$cr = 0;
+		while($row = mysqli_fetch_assoc($result)){
+			//$parent[$cr]['NIC'] = $row['NIC'];
+			$parent[$cr]['name'] = $row['pName'];
+			$parent[$cr]['contact'] = $row['pContact'];
 			$cr++;
 		}
 		
