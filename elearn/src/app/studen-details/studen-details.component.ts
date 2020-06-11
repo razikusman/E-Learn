a9411e685ffa2eaccad0d63830279f30554d1c2b
razikusman <razikusman@gmail.com>
2020-06-11 23:31:50 +0530

    sort out buttons added

--- elearn/src/app/studen-details/studen-details.component.ts
+++ elearn/src/app/studen-details/studen-details.component.ts
@@ -24,14 +24,24 @@ export class StudenDetailsComponent implements OnInit {
     });
   }
 
+  select(six){
+    for(let i=0 ; i<this.students.length ; i++){
+      if(this.students[i].grad==6){
+        this.studentsservicce.viewstudent()
+        .subscribe((data:CreateStudent[])=>{
+          this.students = data;
+        });
+      }
+    }
+  }
   //delete student
   delete(students : CreateStudent): void{
     console.log(students.Student_ID);
     this.studentsservicce.deletestudent(students.Student_ID)
     .subscribe(data => {
       this.students = this.students.filter(u => u !== students);
-      
     })
   }
 
+  
 }
